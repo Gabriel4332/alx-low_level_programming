210d6e37e42e0308e165bffa17dc67fcc595363f
Gabriel4332 <ogokpalaobi@gmail.com>
2023-03-17 11:04:29 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printf function followed by a new line
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}	
